b2de9420a0ff0d6555787c20c8a71437a99b9462
tamerkat <mahad10taha@gmail.com>
2024-03-27 01:34:47 +0200

    update

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Inserts a node as a left
+ * binary_tree_inorder - Inserts a node as a left
  * @tree: A pointer
  * @func: A function
  * Return: sussucceed
@@ -9,10 +9,10 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
-	{
-		binary_tree_inorder(tree->left, func);
-		func(tree->n)
-		binary_tree_inorder(tree->right, func);
-	}
+    if (tree && func)
+    {
+        binary_tree_inorder(tree->left, func);
+        func(tree->n);
+        binary_tree_inorder(tree->right, func);
+    }
 }
