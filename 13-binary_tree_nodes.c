3662417ea41428fe133cbf3ea609e862edbf158a
tamerkat <mahad10taha@gmail.com>
2024-03-27 11:46:10 +0200

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left && tree->right)
-		1 + binary_tree_node(tree->left) + binary_tree_node(tree->right);
-
-	return (binary_tree_nodes(tree->left) + binary_tree_node(tree->right));
+		return (1 + binary_tree_node(tree->left) + binary_tree_node(tree->right));
+	else
+		return (binary_tree_nodes(tree->left) + binary_tree_node(tree->right));
 }
