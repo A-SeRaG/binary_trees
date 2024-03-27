2ab512777bec561d759f1ff38da519996098bb21
tamerkat <mahad10taha@gmail.com>
2024-03-27 14:01:36 +0200

    update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,12 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	binary_tree_t *par = node->parent;
-
-	if (par->left == node)
-		return (par->right);
-	else if (par->right == node)
-		return (par->left);
-	else
-		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
