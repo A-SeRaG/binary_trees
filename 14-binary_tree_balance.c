31e93bc86768159d8a6d750fb9a90fd112b6baf4
tamerkat <mahad10taha@gmail.com>
2024-03-27 14:19:24 +0200

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,34 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Inserts a node as a left
+ * binary_tree_balance - mesure of tree
  * @tree: A pointer
- * Return: sussucceed
-*/
-
+ * Return: tree
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) -
+				binary_tree_height(tree->right));
+	}
 
-	return (height(tree->left) - height(tree->right));
+	return (0);
 }
 
-
 /**
- * height - Inserts a node as a left
+ * binary_tree_height - height of a tree.
  * @tree: A pointer
- * Return: sussucceed
-*/
-
-int height(const binary_tree_t *tree)
+ * Return: tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
+	if (tree)
+	{
+		size_t left = 0, right = 0;
 
-	if (!tree)
-		return (0);
-
-	height_left = tree->left ? 1 + height(tree->left) : 0;
-	height_right = tree->right ? 1 + height(tree->right) : 0;
-	return ((height_left > height_right) ? height_left : height_right);
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((left > right) ? left : right);
+	}
+	return (0);
 }
